09a18b130467eda45c551a695ddee9446a278c03
WalterFrongia <walter.frongia@libero.it>
2021-09-08 16:11:05 +0200

    Implementazione oggetto carta con nome e costo di lancio

---
+++ myscript.js
@@ -0,0 +1,13 @@
+var card = {
+    name : "",//nome della carta
+    launchCost :{ //Creare un oggetto con dentro le caratteristiche che indicano il costo di lancio della carta.
+        common : 2, 
+        white: 3,
+        ublue: 1,
+        black: 4, 
+        red: 0,
+        green:0,
+    }, 
+
+
+};
