696f619c3798347fcfbc4591255832c2f4938899
WalterFrongia <walter.frongia@libero.it>
2021-09-08 16:30:20 +0200

    Implementazione funzione per calcolare i mana della carta sommando gli oggetti in launchCost

--- myscript.js
+++ myscript.js
@@ -1,13 +1,21 @@
 var card = {
     name : "",//nome della carta
-    launchCost :{ //Creare un oggetto con dentro le caratteristiche che indicano il costo di lancio della carta.
+    launchCost :{ //Creare un oggetto con dentro le caratteristiche che indicano il costo di lancio della carta
         common : 2, 
         white: 3,
         ublue: 1,
         black: 4, 
         red: 0,
         green:0,
-    }, 
-
+    },
+    combinedManaCost : function(){ //funzione per calcolare i mana cost
+        let sum = 0;
+        for (let type in this.launchCost){ //this.launchCost serve per prendere ogni elemento dentro launchCost oggetto
+            sum += this.launchCost[type]; //ogni oggetto dentro l'oggetto sommato a tutti i valori degli oggetti
+        }
+        return sum; //per ritornare la somma della funzione
+    }
 
 };
+console.log(card);
+console.log(card.combinedManaCost());
